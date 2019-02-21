4b3e8b62b52f958c75a932250eb2dfd1c0c9cf17
Philipp Tessenow <philipp@tessenow.org>
2019-02-21 12:14:28 +0100

    created empty gem skeleton

---
+++ lib/sql_to_csv_stream.rb
@@ -0,0 +1,6 @@
+require "sql_to_csv_stream/version"
+
+module SqlToCsvStream
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sql_to_csv_stream/version.rb
@@ -0,0 +1,3 @@
+module SqlToCsvStream
+  VERSION = "0.1.0"
+end

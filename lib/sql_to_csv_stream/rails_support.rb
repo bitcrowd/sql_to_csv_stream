377cf0aabb20e7cc329e41aeb52951f60ae6b6b5
Philipp Tessenow <philipp@tessenow.org>
2019-02-26 11:15:00 +0100

    found an easier way to stream data to the browser

--- lib/sql_to_csv_stream/rails_support.rb
+++ lib/sql_to_csv_stream/rails_support.rb
@@ -8,64 +8,50 @@ module SqlToCsvStream
     end
 
     def self.register_csv_renderer
+      # `stream` can be anything that responds to `.each`.
+      # We make use of this by providing our own stream that fetches data from Postgresql
+      # stream = (1..10).lazy.map do |i|
+      #   sleep(1);
+      #   "#{i}\n";
+      # end
+      # stream = Enumerator.new do |strean|
+      #   csv_service.call do |csv_line|
+      #     strean << csv_line
+      #   end
+      # end
       ActionController::Renderers.add :csv_stream do |sql, options|
-        stream_options = SqlToCsvStream::RailsSupport.prepare_streaming(
-          'text/csv',
-          request,
-          response,
-          headers,
-          options
+        copy_options = options.delete(:copy_options) || {}
+        stream = CsvStream.new(
+          sql,
+          use_gzip: SqlToCsvStream::RailsSupport.use_gzip?(request),
+          copy_options: copy_options
         )
-        self.response_body = CsvStream.new(sql, **stream_options)
+        SqlToCsvStream::RailsSupport.set_streaming_headers(headers, request)
+        send_data stream, **options
       end
     end
 
     def self.register_json_renderer
       ActionController::Renderers.add :json_stream do |sql, options|
-        stream_options = SqlToCsvStream::RailsSupport.prepare_streaming(
-          'application/json',
-          request,
-          response,
-          headers,
-          options
-        ).slice(:use_gzip)
-        self.response_body = JsonStream.new(sql, **stream_options)
+        stream = JsonStream.new(
+          sql,
+          use_gzip: SqlToCsvStream::RailsSupport.use_gzip?(request)
+        )
+        SqlToCsvStream::RailsSupport.set_streaming_headers(headers, request)
+        send_data stream, **options
       end
     end
 
-    def self.default_headers(filename, type)
-      {
-        'Content-Type' => "#{type}; charset=utf-8",
-        'Content-Disposition' => "attachment; filename=\"#{filename}\"",
-        # from nginx doc: Setting this to "no" will allow unbuffered responses
-        # suitable for HTTP streaming applications.
-        'X-Accel-Buffering' => 'no',
-        'Cache-Control' => 'no-cache'
-      }
+    def self.set_streaming_headers(headers, request)
+      headers['X-Accel-Buffering'] = 'no'
+      headers['Cache-Control'] = 'no-cache'
+      headers['Content-Encoding'] = 'gzip' if use_gzip?(request)
     end
 
-    def self.prepare_streaming(type, request, response, response_headers, options)
-      copy_options = options.fetch(:copy_options, {})
-      apply_headers(options, type, response_headers)
-      response.status = options.fetch(:status, 200)
-
-      if request.headers['HTTP_ACCEPT_ENCODING'].to_s.include?('gzip')
-        response_headers['Content-Encoding'] = 'gzip'
-        return { copy_options: copy_options, use_gzip: true }
-      end
-
-      { copy_options: copy_options, use_gzip: false }
-    end
-
-    def self.apply_headers(options, type, response_headers)
-      filename = options.fetch(:filename, 'data')
-      user_defined_headers = options.fetch(:headers, {})
-
-      merged_headers = default_headers(filename, type).merge(user_defined_headers)
-      merged_headers.each do |header_name, header_value|
-        response_headers[header_name.to_s] = header_value.to_s
-      end
-      response_headers.delete('Content-Length') unless merged_headers['Content-Length']
+    def self.use_gzip?(request)
+      request.headers['HTTP_ACCEPT_ENCODING']
+             .to_s
+             .include?('gzip')
     end
   end
 end
